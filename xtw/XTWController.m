1595256d5ed9306a6cb409e95c52b77f29056c12
Andy Grant <andywgrant@gmail.com>
2015-09-25 10:31:16 -0700

    Underline looks better than highlighting for in progress tasks

--- xtw/XTWController.m
+++ xtw/XTWController.m
@@ -115,7 +115,7 @@
         }
         
         if(task[@"start"]) {
-            [attributedTitle addAttribute:NSBackgroundColorAttributeName value:[NSColor lightGrayColor] range:NSMakeRange(0,[taskMI title].length)];
+            [attributedTitle addAttribute:NSUnderlineStyleAttributeName value:@(NSUnderlineStyleThick) range:NSMakeRange(0,[taskMI title].length)];
         }
         
         NSMenu *submenu = [[NSMenu alloc] init];
